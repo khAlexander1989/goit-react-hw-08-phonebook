7516102a18ae045acf5957741504040dd679cac6
Alexander Khardin <khporter1984@gmail.com>
2022-09-29 13:00:14 +0300

    phonebook app init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,5 @@
+import { Box } from 'components/Box';
+
+export const App = () => {
+  return <Box p={2}>React homework template</Box>;
+};
---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { layout, space, flexbox, color, borders, shadow } from 'styled-system';
+
+export const Box = styled.div`
+  ${layout}
+  ${flexbox}
+  ${space}
+  ${color}
+  ${borders}
+  ${shadow}
+`;
