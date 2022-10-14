7520afe6d41837b44eec598d017c0670b1881cde
Alexander Khardin <khporter1984@gmail.com>
2022-10-14 17:33:25 +0300

    refacto phonebook app by use hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Box } from 'components/Box';
@@ -9,41 +9,37 @@ import { ContactsTitle, AppTitle } from './App.styled';
 import * as LSApi from 'utils/localeStorageApi';
 import { LS_DATA_KEY } from 'utils/constants';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  componentDidMount() {
+export function App() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
     const serializedContacts = LSApi.getDataFromLocalStorage(LS_DATA_KEY);
-    if (serializedContacts) {
-      const deserializedContacts = LSApi.deserializeData(serializedContacts);
-      this.setState({ contacts: deserializedContacts });
-    }
+
+    return serializedContacts
+      ? LSApi.deserializeData(serializedContacts)
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
+
+  useEffect(() => {
+    LSApi.addDataToLocalStorage(LS_DATA_KEY, LSApi.serializeData(contacts));
+  }, [contacts]);
+
+  function changeFilterValue(value) {
+    setFilter(value);
   }
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      const serializedContacts = LSApi.serializeData(this.state.contacts);
-      LSApi.addDataToLocalStorage(LS_DATA_KEY, serializedContacts);
-    }
-  }
-
-  changeFilterValue = value => this.setState({ filter: value });
-
-  isExistContact = (contactName, contacts) =>
+  function isExistContact(contactName, contacts) {
     contacts.some(
       ({ name }) => name.toUpperCase() === contactName.toUpperCase()
     );
+  }
 
-  addContact = ({ name, number }) => {
-    if (this.isExistContact(name, this.state.contacts)) {
+  function addContact({ name, number }) {
+    if (isExistContact(name, contacts)) {
       alert(`${name} is already in contacts`);
       return;
     }
@@ -54,52 +50,43 @@ export class App extends Component {
       number,
     };
 
-    this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
-  };
+    setContacts(prevContacts => [newContact, ...prevContacts]);
+  }
 
-  deleteContact = id => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  filterContacts = keyword => {
-    return this.state.contacts.filter(({ name }) =>
-      name.toUpperCase().includes(keyword.toUpperCase())
-    );
-  };
-
-  orderContacts = contacts => {
-    return [...contacts].sort((a, b) => a.name.localeCompare(b.name));
-  };
-
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.filterContacts(filter);
-    const orderedContacts = this.orderContacts(filteredContacts);
-
-    return (
-      <Box
-        p={2}
-        width="450px"
-        border="default"
-        borderRadius="normal"
-        borderColor="borderLightGrey"
-      >
-        <AppTitle>Phonebook</AppTitle>
-        <ContactForm onContactFormSubmit={this.addContact} />
-
-        <ContactsTitle>Contacts</ContactsTitle>
-        <Filter
-          onFilterChange={this.changeFilterValue}
-          filterValue={this.state.filter}
-        />
-
-        <ContactList
-          contacts={orderedContacts}
-          onContactDelete={this.deleteContact}
-        />
-      </Box>
+  function deleteContact(id) {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
   }
+
+  function filterContacts(keyword) {
+    return contacts.filter(({ name }) =>
+      name.toUpperCase().includes(keyword.toUpperCase())
+    );
+  }
+
+  function orderContacts(contacts) {
+    return [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  return (
+    <Box
+      p={2}
+      width="450px"
+      border="default"
+      borderRadius="normal"
+      borderColor="borderLightGrey"
+    >
+      <AppTitle>Phonebook</AppTitle>
+      <ContactForm onContactFormSubmit={addContact} />
+
+      <ContactsTitle>Contacts</ContactsTitle>
+      <Filter onFilterChange={changeFilterValue} filterValue={filter} />
+
+      <ContactList
+        contacts={orderContacts(filterContacts(filter))}
+        onContactDelete={deleteContact}
+      />
+    </Box>
+  );
 }
