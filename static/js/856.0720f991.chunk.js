d1542eb3adf6ee351eb0a41da771f6491e2f18e9
khAlexander1989 <98282740+khAlexander1989@users.noreply.github.com>
2022-11-21 21:19:31 +0000

    Deploying to gh-pages from @ khAlexander1989/goit-react-hw-08-phonebook@4eaa8ab2b8c0bbe93895e6fbe5505c6c5eb7f165 🚀

---
+++ static/js/856.0720f991.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[856],{6856:function(t,e,a){a.d(e,{rnx:function(){return h}});var r=a(9983);function h(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M3 10h11v2H3v-2zm0-2h11V6H3v2zm0 8h7v-2H3v2zm15.01-3.13l.71-.71a.996.996 0 011.41 0l.71.71c.39.39.39 1.02 0 1.41l-.71.71-2.12-2.12zm-.71.71l-5.3 5.3V21h2.12l5.3-5.3-2.12-2.12z"}}]})(t)}}}]);
+//# sourceMappingURL=856.0720f991.chunk.js.map
